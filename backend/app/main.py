4f9f033bf7d2064752773eca0823a5b4b9a05f2f
Divyansh15322 <idivyansh88@gmail.com>
2026-06-11 05:28:45 +0530

    fix cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://url-shortner-267s67e70-divyansh15322s-projects.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
